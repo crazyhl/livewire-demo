3b077e39aefb0491f26da04ef4234e64ca2d488a
M1racle <crazyhl@163.com>
2022-10-29 13:46:19 +0800

    优化 简单调整一下样式

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     @livewireStyles
 </head>
 <body>
+    <div class="font-mono container mx-auto px-4">
     {{ $slot }}
+    </div>
     @livewireScripts
 </body>
 </html>
--- resources/views/livewire/post/show.blade.php
+++ resources/views/livewire/post/show.blade.php
@@ -1,9 +1,8 @@
 <div>
     {{-- Care about people's approval and you will be their prisoner. --}}
     <div class="p-1 border border-slate-200 rounded-md mb-5">
-        <div>{{$post->id}}</div>
-        <div>{{$post->title}}</div>
-        <div>{{$post->user->name}}</div>
+        <div>{{$post->title}} - {{$post->id}}</div>
+        <div class="text-sm">{{$post->user->name}}</div>
         <div>{{$post->content}}</div>
     </div>
 </div>
